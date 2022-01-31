69bd35b1f1f6210f01246a278ec1b986d4e62209
Joel Cujilema <joelca1945@gmail.com>
2022-01-31 00:04:28 -0500

    cors agregados

--- app.js
+++ app.js
@@ -1,11 +1,13 @@
 const express = require('express');
 const app = express();
+const cors = require('cors');
 
 //Cargar Rutas
 const deudoresRoutes = require('./routes/deudor.route')
 const cobradoresRoutes = require('./routes/cobrador.route')
 const pagosRoutes = require('./routes/pago.route')
 
+app.use(cors());
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
 
